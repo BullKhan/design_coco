3ff966d84b316815f0eb146de3b4ba77c617713e
BullKhan <77268926+BullKhan@users.noreply.github.com>
2021-02-15 17:08:12 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -16,6 +16,12 @@ $(".slide_group").slick({
     pauseOnFocus: false, // 동그라미번호버튼 클릭시 자동실행 멈춤여부
     prevArrow: '<button class="prev"><i class="fas fa-angle-left"></i></button>',
     nextArrow: '<button class="next"><i class="fas fa-angle-right"></i></button>',
+    responsive: [{
+        breakpoint: 1025,
+        settings: {
+            arrows: false,
+        }
+    }]
 })
 
 $('.article1 .plpa').on('click', function(){
@@ -41,20 +47,74 @@ $('.article5 .tabmenu > li').on('click', function(){
     .siblings().removeClass('active')
 })
 
-
-var ww = $(window).width();
-console.log(ww)
-if ( ww > 1024 ) {
-    $('#nav .depth1 > li').hover(
-        function(){ $(this).addClass('on') },
-        function(){ $(this).removeClass('on') }
-    )
-} else {
-    $('#nav .depth1 > li').on('click',function(){
-        if ( $(this).hasClass('on') ) {
-            $(this).removeClass('on')
-        } else {
-            $(this).addClass('on').siblings().removeClass('on')
-        }
-    })
+// 여기서부터 resize 이벤트 발생시 스크롤바 유무에 따라 상태(화면너비)제어 프로그램
+var deviceSize = 1024;
+function scrollOX(status) {
+    $('html').css({ overflowY:status })
+    var htmlWidth = $('html').width();
+    return htmlWidth
 }
+var swh = scrollOX('hidden');
+var sws = scrollOX('scroll');
+var swd = swh - sws;
+if ( swd > 0 ) {
+    deviceSize = deviceSize - swd;
+}
+
+// 함수 선언
+function calc_width(){
+    var ww = $(window).width();
+    if ( ww > deviceSize ) {
+        $('html').addClass('pc').removeClass('mobile')
+        $('.search #sbox').removeClass('on')
+        $('.depth1 > li').removeClass('on')
+        $('#header .nh button').find('i').removeClass('fa-times').addClass('fa-bars')
+    } else if ( ww <= deviceSize && !$('html').hasClass('mobile') ){
+        $('html').addClass('mobile').removeClass('pc')
+        $('#nav').removeClass('on')
+        $('.search #sbox').removeClass('on')
+    }
+}
+
+
+calc_width();
+$(window).on('resize', function(){ calc_width(); })
+
+$('.depth1 > li').hover(
+    function(){
+        if ( $('html').hasClass('pc') ) {
+            $(this).addClass('on')
+        }
+    },
+    function(){
+        if ( $('html').hasClass('pc') ) {
+            $(this).removeClass('on')
+        }
+    }
+)
+$('.depth1 > li').on('click',
+    function(e){
+        if ( $('html').hasClass('mobile') ) {
+            e.preventDefault();
+            $(this).toggleClass('on').siblings().removeClass('on')
+        }
+    }
+)
+$('.depth2 > li').on('click',function(e){
+    e.stopPropagation();
+})
+
+
+// 돋보기 클릭시 검색창 박스 여닫기
+$('.search label').on('click', function(){
+    $(this).prev().toggleClass('on')
+})
+
+$(window).on('scroll', function(){
+    var sct = $(this).scrollTop();
+    if ( sct>=1 && !$('#header').hasClass('on') ) {
+        $('#header').addClass('on')
+    } else if ( sct< 1 && $('#header').hasClass('on') ) {
+        $('#header').removeClass('on')
+    }
+})
